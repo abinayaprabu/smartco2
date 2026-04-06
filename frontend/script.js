4370085640c66f629560cff2e9ede153ec13d662
abinayaprabu <abinaya2310921@ssn.edu.in>
2026-04-06 20:31:44 +0530

    updated backend url

--- frontend/script.js
+++ frontend/script.js
@@ -40,7 +40,7 @@ function predictCO2() {
         return;
     }
 
-    fetch("http://127.0.0.1:5000/predict", {
+    fetch("https://smartco2-backend.onrender.com/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
